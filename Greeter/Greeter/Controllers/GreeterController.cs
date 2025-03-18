514614ae4cf88c1f8ab32de0d0c0e7f2e55b29b7
0marKh1i1 <okkhalil23@cit.just.edu.jo>
2025-03-18 15:52:53 +0300

    Task 44Create

---
+++ Greeter/Greeter/Controllers/GreeterController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Greeter.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GreeterController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get([FromQuery] string name = "anonymous")
+        {
+            return Ok($"Hello {name}");
+        }
+    }
+}
